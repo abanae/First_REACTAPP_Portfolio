64b7116b55d1e58fdee72501c81114654f2d5fce
Ana Ea <aabad0464@gmail.com>
2021-09-08 13:56:00 -0600

    creating a layout.js

--- src/timelineElements.js
+++ src/timelineElements.js
@@ -1,11 +1,11 @@
 let timelineElements = [
   {
     id: 1,
-    title: "University of Denver, University of Denver Coding Bootcamp",
+    title: "CURRENT STUDENT at University of Denver, University of Denver Coding Bootcamp",
     location: "Denver, Colorado",
     description:
       "*24 weeks of immersive experience that teaches Junior level web developers the skills needed to be successful. *Completed daily challenges & weekly projects gaining experience in full-stack development and deployment. *Practice pair-programing, test driven development, and agile methodologies",
-      buttonText: "Course Certificate",
+      // buttonText: "Course Certificate",
       date: "April 2021 - Current",
     icon: "school",
   },
@@ -15,9 +15,12 @@ let timelineElements = [
       location: "BootCamp, University of Denver",
       description:
         "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
-      buttonText: "View Frontend Projects",
       date: "May 2021",
       icon: "work",
+      link :{
+        url: 'https://abanae.github.io/Password_Generator/',
+        buttonText: "View Frontend Projects",
+      }
     },
     {
       id: 3,
@@ -25,9 +28,13 @@ let timelineElements = [
       location: "BootCamp, University of Denver",
       description:
         "Creating the logic to make the web app function properly, and accomplishing this through the use of server-side scripting languages. DESCRIPTION : Deepen your lyrical knowledge by using our dictionary functions to look up any 'slang' term from your favorite songs.Or further your 'traditional' knowledge with our Webster dictionary search.",
-      buttonText: "View Backend Projects",
+      // buttonText: "View Backend Projects",
       date: "June 2021",
       icon: "work",
+      link :{
+        url: 'https://maximosandoval.github.io/urban-cipher/',
+        buttonText: "View Backend Projects",
+      }
     },
     {
       id: 4,
@@ -35,9 +42,13 @@ let timelineElements = [
       location: "BootCamp, University of Denver",
       description:
         "A Blog created with mysql database, CMS-style, Model View Controller (MVC) paradigm. User is required to sign up to create posts or comments to the blog. User can also update & delete comments/posts.",
-      buttonText: "View Backend Projects",
+      // buttonText: "View Backend Projects",
       date: "July 2021",
       icon: "work",
+      link :{
+        url: 'https://awesome-blogger.herokuapp.com/',
+        buttonText: "View Backend Projects",
+      }
     },
     {
       id: 5,
@@ -45,9 +56,13 @@ let timelineElements = [
       location: "BootCamp, University of Denver",
       description:
         "Our project is a mock e-commerce store called Really Good Fake Store. E-commerce websites make up about 10% of the internet. Our goal was to be able to build this mock store with our current coding skills to mock something similar to what we might see on a real job.The client has the ability to create an account, login/logout, and add/delete products to their shpping cart.",
-      buttonText: "View Backend Projects",
+      // buttonText: "View Backend Projects",
       date: "August 2021",
       icon: "work",
+      link :{
+        url: 'https://really-good-fake-store.herokuapp.com/',
+        buttonText: "View Backend Projects",
+      }
     },
     {
       id: 6,
@@ -55,11 +70,11 @@ let timelineElements = [
       location: "BootCamp, University of Denver",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-      buttonText: "View Backend Projects",
+      // buttonText: "View Backend Projects",
       date: "Currently Working On It",
       icon: "work",
     },
-    
+   
   ]
   
   export default timelineElements
